ba10a1cbc6e9ca6b3bf5116c22f54dcdd2a165ef
gerlit <165931607+gerlit@users.noreply.github.com>
2024-04-18 19:47:24 +0300

    Find name in list.java

---
+++ Find name in list.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String[] guests = {"Mart", "Kalev", "Annika", "Laura"};
+    
+    System.out.print("Check if your on the list: ");
+    String answer = scanner.nextLine();
+
+    boolean isFound = false;
+    
+    for (int i = 0; i < guests.length; i++) {
+      if (answer.equals(guests[i])) {
+        isFound = true;
+        System.out.println(answer + " is on the list.");
+        break;
+      } 
+    }
+
+    if (isFound == false){
+      System.out.println(answer + " is not on the list.");
+    }
+    
+    scanner.close();
+  }
+}
