a58a59eadb54bba61c04b28b06fcc6a062e3ad20
gerlit <165931607+gerlit@users.noreply.github.com>
2024-04-24 22:43:26 +0300

    NEW methods, sum.java

---
+++ NEW methods, sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Please provide a number: ");
+    int number1 = scanner.nextInt();
+    
+    System.out.print("Please provide another number: ");
+    int number2 = scanner.nextInt();
+    
+    int result = sum(number1, number2);
+
+    System.out.print("Adding these two numbers makes: ");
+    System.out.println(result);
+  }
+
+  public static int sum (int number1, int number2) {
+    return number1 + number2;
+  }
+}
