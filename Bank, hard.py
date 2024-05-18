f1e8c8ad23e33aac6b053e742a654c1d67680060
gerlit <165931607+gerlit@users.noreply.github.com>
2024-05-18 13:42:44 +0300

    Bank, hard.py

---
+++ Bank, hard.py
@@ -0,0 +1,29 @@
+print("Welcome to the Bank")
+
+balance = 0
+
+
+while True:
+  answer = input("\nWould you like to?: \n1. Deposit to your account\n2. Withdraw from your account\n3. Check balance\n4. Exit\n")
+  if answer == "1":
+    try:
+      deposit = float(input("\nHow much would you like to deposit to your bank account? "))
+      balance = balance + deposit
+    except:
+      print("Invalid input. Please enter a numeric value for the deposit.")
+  elif answer == "2":
+    try:
+      withdraw = float(input("\nHow much would you like to withdraw from your bank account? "))
+      if withdraw > balance:
+        print("You do not have enough funds on your bank account")
+      else:
+        balance = balance - withdraw
+    except:
+      print("Invalid input. Please enter a numeric value for the deposit.")
+  elif answer == "3":
+    print(f"\nYour total account balance is {round(balance, 2)}")
+  elif answer == "4":
+    print(f"\nHave a nice day!")
+    break
+  else:
+    print("\nPlease enter a number from these 4 options")
