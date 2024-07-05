374912062a258698f9363fb3290539b70eaabe7e
gerlit <165931607+gerlit@users.noreply.github.com>
2024-07-05 15:50:34 +0300

    Python prices.py

--- Python prices.py
+++ Python prices.py
@@ -1,1 +1,18 @@
+filename = "/content/prices.txt"
 
+with open(filename, "r") as file:
+    lines = file.readlines()
+
+total_price = 0
+total_items = 0
+
+for line in lines:
+    try:
+        price = float(line)
+        total_price += price
+    except ValueError:
+        total_items += 1
+        continue
+
+print(f"Total Price: {total_price}")
+print(f"Total Number of Items: {total_items}")
